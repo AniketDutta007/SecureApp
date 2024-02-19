25c45d3eb89d2039fbfbdbb72922777c5caf11e4
AniketDutta007 <aniket2018dutta@gmail.com>
2024-02-19 23:28:09 +0530

    updated application.properties

--- src/main/java/com/nrifintech/training/secureapp/models/User.java
+++ src/main/java/com/nrifintech/training/secureapp/models/User.java
@@ -17,7 +17,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = {"email", "role"}))
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
